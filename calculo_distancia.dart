ee10226d78db979bfdcf431d075f17e6b227330b
jehpereira2017 <je.mary0804@gmail.com>
2020-12-16 17:44:14 -0300

    incluindo testes

--- calculo_distancia.dart
+++ calculo_distancia.dart
@@ -1,5 +1,7 @@
 import 'dart:math' as math;
 
+import 'package:test/test.dart';
+
 class Rotas {
   int coordenadaX;
   int coordenadaY;
@@ -8,28 +10,66 @@ class Rotas {
   Rotas({this.coordenadaX, this.coordenadaY, this.distancia});
 
   @override
-  String toString() => 'Rotas($coordenadaX, $coordenadaY)';
+  String toString() => '[$coordenadaX,$coordenadaY]';
+}
+
+void calculaDistancia({Rotas cliente, List<Rotas> lojas}) {
+  assert(
+    cliente.coordenadaX != null,
+    'coordenada X esta nula',
+  );
+  Rotas _cliente = cliente;
+  List<Rotas> _lojas = lojas;
+
+  for (int i = 0; i < _lojas.length; i++) {
+    var calculateX = _lojas[i].coordenadaX - _cliente.coordenadaX;
+    var calculateY = _lojas[i].coordenadaY - _cliente.coordenadaY;
+    var elevateX = math.pow(calculateX, 2);
+    var elevateY = math.pow(calculateY, 2);
+    _lojas[i].distancia = math.sqrt(elevateX + elevateY);
+  }
+}
+
+void ordenaLista(List<Rotas> lojas) {
+  lojas.sort((a, b) => a.distancia.compareTo(b.distancia));
 }
 
 void main() {
-  Rotas cliente = new Rotas(coordenadaX: 5, coordenadaY: 10, distancia: 0);
-  List<Rotas> lojas = List<Rotas>();
-  lojas.add(Rotas(coordenadaX: 40, coordenadaY: 88, distancia: 0));
-  lojas.add(Rotas(coordenadaX: 18, coordenadaY: 56, distancia: 0));
-  lojas.add(Rotas(coordenadaX: 99, coordenadaY: 2, distancia: 0));
-  lojas.add(Rotas(coordenadaX: 14, coordenadaY: 99, distancia: 0));
-  lojas.add(Rotas(coordenadaX: 10, coordenadaY: 2, distancia: 0));
-  lojas.add(Rotas(coordenadaX: 15, coordenadaY: 100, distancia: 0));
+  test('teste da centauro', () {
+    // Arrange
+    Rotas cliente;
+    List<Rotas> lojas = List<Rotas>();
 
-  for (int i = 0; i < lojas.length; i++) {
-    var calculateX = lojas[i].coordenadaX - cliente.coordenadaX;
-    var calculateY = lojas[i].coordenadaY - cliente.coordenadaY;
-    var elevateX = math.pow(calculateX, 2);
-    var elevateY = math.pow(calculateY, 2);
-    lojas[i].distancia = math.sqrt(elevateX + elevateY);
-    print(lojas[i].distancia);
-  }
+    cliente = Rotas(coordenadaX: 20, coordenadaY: 32, distancia: 0);
+    lojas.add(Rotas(coordenadaX: 40, coordenadaY: 88, distancia: 0));
+    lojas.add(Rotas(coordenadaX: 18, coordenadaY: 56, distancia: 0));
+    lojas.add(Rotas(coordenadaX: 99, coordenadaY: 2, distancia: 0));
 
-  lojas.sort((a, b) => a.distancia.compareTo(b.distancia));
-  print(lojas.toString());
+    // Act
+    calculaDistancia(cliente: cliente, lojas: lojas);
+    ordenaLista(lojas);
+
+    // Assert
+    expect(lojas.toString(), '[[18,56], [40,88], [99,2]]');
+  });
+
+  test('teste cliente com coordenada null', () {
+    // Arrange
+    Rotas cliente;
+    List<Rotas> lojas = List<Rotas>();
+    bool assertionError = false;
+    cliente = Rotas(coordenadaX: null, coordenadaY: 32, distancia: 0);
+    lojas.add(Rotas(coordenadaX: 40, coordenadaY: 88, distancia: 0));
+    lojas.add(Rotas(coordenadaX: 18, coordenadaY: 56, distancia: 0));
+    lojas.add(Rotas(coordenadaX: 99, coordenadaY: 2, distancia: 0));
+
+    try {
+      calculaDistancia(cliente: cliente, lojas: lojas);
+    } on AssertionError {
+      assertionError = true;
+    }
+
+    // Assert
+    expect(assertionError, true);
+  });
 }
