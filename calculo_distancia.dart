692644d89348d4e66b7cdb6546f860fe045dcd6e
jehpereira2017 <je.mary0804@gmail.com>
2020-12-16 17:52:17 -0300

    mais testes

--- calculo_distancia.dart
+++ calculo_distancia.dart
@@ -14,14 +14,12 @@ class Rotas {
 }
 
 void calculaDistancia({Rotas cliente, List<Rotas> lojas}) {
-  assert(
-    cliente.coordenadaX != null,
-    'coordenada X esta nula',
-  );
+  assert(cliente.coordenadaX != null && cliente.coordenadaX != null);
   Rotas _cliente = cliente;
   List<Rotas> _lojas = lojas;
 
   for (int i = 0; i < _lojas.length; i++) {
+    assert(_lojas[i].coordenadaX != null && _lojas[i].coordenadaY != null);
     var calculateX = _lojas[i].coordenadaX - _cliente.coordenadaX;
     var calculateY = _lojas[i].coordenadaY - _cliente.coordenadaY;
     var elevateX = math.pow(calculateX, 2);
@@ -61,6 +59,26 @@ void main() {
     cliente = Rotas(coordenadaX: null, coordenadaY: 32, distancia: 0);
     lojas.add(Rotas(coordenadaX: 40, coordenadaY: 88, distancia: 0));
     lojas.add(Rotas(coordenadaX: 18, coordenadaY: 56, distancia: 0));
+    lojas.add(Rotas(coordenadaX: 99, coordenadaY: 2, distancia: 0));
+
+    try {
+      calculaDistancia(cliente: cliente, lojas: lojas);
+    } on AssertionError {
+      assertionError = true;
+    }
+
+    // Assert
+    expect(assertionError, true);
+  });
+
+  test('teste loja com coordenada null', () {
+    // Arrange
+    Rotas cliente;
+    List<Rotas> lojas = List<Rotas>();
+    bool assertionError = false;
+    cliente = Rotas(coordenadaX: 20, coordenadaY: 32, distancia: 0);
+    lojas.add(Rotas(coordenadaX: 40, coordenadaY: 88, distancia: 0));
+    lojas.add(Rotas(coordenadaX: null, coordenadaY: 56, distancia: 0));
     lojas.add(Rotas(coordenadaX: 99, coordenadaY: 2, distancia: 0));
 
     try {
